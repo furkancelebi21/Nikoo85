1e482d99bab73d744532cfc25c21fc8dd70cec17
Asus <Asus@FURKAN>
2022-01-10 21:53:24 +0200

    Proje dosyası ekle.

---
+++ furkii/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace furkii.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index(String email,String pw)
+        {
+
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
